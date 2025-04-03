066ae3cbaf3990a4e0c41920c293c991ec84d22d
juanrojas <juanfontalvorojas@gmail.com>
2025-04-03 11:18:28 -0500

    Nuevo

--- login.py
+++ login.py
@@ -5,11 +5,12 @@ ventana = tk.Tk()
 def pantalla():
     ventana.geometry("800x600") #Ancho x Alto
     ventana.title("Proyecto")#Titulo
+    
     ventana.resizable(False,False)#Para no Poder estirar la pantalla
 pantalla()
 
 ventana.mainloop() #Este llama a la ventana para poder verla
-#Geometry para darle el tamaño a la pantalla
+
 
 
 
