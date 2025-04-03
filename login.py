3f14cddbbb3bf5e25359b04084b86c6c5b261c58
juanrojas <juanfontalvorojas@gmail.com>
2025-04-03 09:16:51 -0500

    Primer Comit

---
+++ login.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+
+def pantalla():
+    ventana.geometry("800x600") #Ancho x Alto
+pantalla()
+ventana.mainloop() #Este llama a la ventana para poder verla
+#Geometry para darle el tamaño a la pantalla
